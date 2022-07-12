9e22bd7ab1f96c0736f6e0aa668234f0a8521f5b
abril-bazann <abrilbazan28@gmail.com>
2022-07-12 13:41:44 -0300

    agrego templates con css con bootstrap y las modifico (clase19)

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -13,6 +13,22 @@ def curso(self):
 
     return HttpResponse(texto)
 
+def inicio(request):
+    return render (request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return render (request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render (request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render (request, "AppCoder/entregables.html")
+
+
 def mama(self):
     nombre='Daniela'
     apellido='Lascano'
